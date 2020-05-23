41a4b857bd58ededb3f1ae551fbcfa9a98b80207
Michael Randers-Pehrson <michael.rp@gmail.com>
2020-05-22 21:54:50 -0400

    Add test project

--- TimeCalc/Services/SolveCalculator.cs
+++ TimeCalc/Services/SolveCalculator.cs
@@ -6,7 +6,42 @@ namespace TimeCalc.Services
     {
         public SolveCalculations GetCalculations(Solve[] solves, string pb)
         {
-            return new SolveCalculations { IncludedSolves = new int[0] };
+            return new SolveCalculations { IncludedSolves = new int[] { 1, 2 } };
         }
+
+        /*
+    private convertResultToSeconds(input: string): number {
+        let result = 0.0;
+
+        // truncate decimal
+        let truncated: string;
+        const decimalParts = input.split('.');
+        if (decimalParts.length > 1) {
+            truncated = decimalParts[0] + '.' + decimalParts[1].substring(0, 2);
+        } else {
+            truncated = decimalParts[0];
+        }
+
+        // convert minutes to seconds and parse to number
+        let converted = 0.0;
+        const timeParts = truncated.split(':');
+        if (timeParts.length === 1) {
+            // seconds
+            converted = parseFloat(timeParts[0]);
+        } else if (timeParts.length === 2) {
+            // minutes, seconds
+            converted = parseFloat(timeParts[0]) * 60 + parseFloat(timeParts[1]);
+        }
+
+        if (!Number.isNaN(converted)) {
+            result = converted;
+        }
+
+        // console.log('convertResultToSeconds', input, result);
+
+        return result;
+    }
+
+        */
     }
 }
