e202118a2143553158d703c22ba65451fb3418eb
Michael Randers-Pehrson <michael.rp@gmail.com>
2020-05-23 09:59:37 -0400

    More calculations

--- TimeCalc.Tests/SolveCalculatorTests.cs
+++ TimeCalc.Tests/SolveCalculatorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 using TimeCalc.Models;
 using TimeCalc.Services;
@@ -7,17 +8,63 @@ namespace TimeCalc.Tests
 {
     public class SolveCalculatorTests
     {
+        // [Theory]
+        // [InlineData("9.342", 934)]
+        // [InlineData(".42", 42)]
+        // [InlineData("43.1", 4310)]
+        // [InlineData("1:02.010", 6201)]
+        // [InlineData("1:0.1", 6010)]
+        // public void ConvertStringToSeconds(string input, int result)
+        // {
+        //     var calc = new SolveCalculator();
+
+        //     Assert.Equal(result, calc.ConvertToCentiseconds(input));
+        // }
+
         [Theory]
-        [InlineData("9.342", 934)]
-        [InlineData(".42", 42)]
-        [InlineData("43.1", 4310)]
-        [InlineData("1:02.010", 6201)]
-        [InlineData("1:0.1", 6010)]
-        public void ConvertStringToSeconds(string input, int result)
+        [InlineData("9.342", 9.34)]
+        [InlineData(".42", 0.42)]
+        [InlineData("43.10", 43.1)]
+        [InlineData("43.1", 43.1)]
+        [InlineData("43.100", 43.1)]
+        [InlineData("43.109", 43.1)]
+        [InlineData("1:02.010", 62.01)]
+        [InlineData("1:0.1", 60.1)]
+        public void ConvertResultToSeconds(string input, float result)
         {
             var calc = new SolveCalculator();
 
-            Assert.Equal(result, calc.ConvertToCentiseconds(input));
+            Assert.Equal(result, calc.ConvertResultToSeconds(input));
+        }
+
+/*
+8.36		7.71	8.77	7.93	9.89	8.39
+43.54		42.63	54.83	38.05	39.73	48.26
+56.62		1:13.91	1:09.15	52.52	48.19	44.33
+1.90		0.68	1.98	1.99	1.93	1.80
+1:06.32		1:06.54	1:01.79	1:35.42	1:03.27	1:09.14
+*/
+        [Theory]
+        [InlineData("7.71", "8.77", "7.93", "9.89", "8.39", 2, 3, 5)]
+        [InlineData("42.63", "54.83", "38.05", "39.73", "48.26", 1, 4, 5)]
+        [InlineData("1:13.91", "1:09.15", "52.52", "48.19", "44.33", 2, 3, 4)]
+        [InlineData("0.68", "1.98", "1.99", "1.93", "1.80", 2, 4, 5)]
+        [InlineData("1:06.54", "1:01.79", "1:35.42", "1:03.27", "1:09.14", 1, 4, 5)]
+        public void GetIncludedSolves(string s1, string s2, string s3, string s4, string s5, params int[] included)
+        {
+            Solve[] solves = {
+                new Solve { Number = 1, Result = s1 },
+                new Solve { Number = 2, Result = s2 },
+                new Solve { Number = 3, Result = s3 },
+                new Solve { Number = 4, Result = s4 },
+                new Solve { Number = 5, Result = s5 }
+            };
+
+            var calc = new SolveCalculator();
+            var result = calc.GetIncludedSolves(solves);
+            var orderedResult = result.OrderBy(r => r.Item1).Select(r => r.Item1).ToArray();
+
+            Assert.Equal(included, orderedResult);
         }
     }    
 }
--- TimeCalc/Services/SolveCalculator.cs
+++ TimeCalc/Services/SolveCalculator.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using TimeCalc.Models;
 
 namespace TimeCalc.Services
@@ -9,20 +12,67 @@ namespace TimeCalc.Services
             return new SolveCalculations { IncludedSolves = new int[] { 1, 2 } };
         }
 
+        public IEnumerable<Tuple<int, float>> GetIncludedSolves(Solve[] solves)
+        {
+            var converted = solves.Where(s => !string.IsNullOrEmpty(s.Result))
+                                  .Select(s => new Tuple<int, float>(s.Number, ConvertResultToSeconds(s.Result)))
+                                  .OrderBy(s => s.Item2);
+
+            return converted.Count() < 4 ? converted : converted.Skip(1).Take(3);
+        }
+
+        public float ConvertResultToSeconds(string input)
+        {
+            var decimalParts = input.Split(".");
+
+            // truncate decimal
+            var truncated = decimalParts[0];
+            if (decimalParts.Length > 1)
+            {
+                truncated += ".";
+                if (decimalParts[1].Length > 2)
+                {
+                    truncated += decimalParts[1].Substring(0, 2);
+                }
+                else
+                {
+                    truncated += decimalParts[1];
+                }
+            }
+
+            var timeParts = truncated.Split(":");
+
+            // convert minutes to seconds and parse to number
+            float converted;
+            if (timeParts.Length == 1)
+            {
+                // seconds
+                converted = float.Parse(timeParts[0]);
+            }
+            else
+            {
+                // minutes, seconds
+                converted = float.Parse(timeParts[0]) * 60 + float.Parse(timeParts[1]);
+            }
+
+            return converted;
+        }
+
+        [Obsolete]
         public int ConvertToCentiseconds(string input)
         {
             var decParts = input.Split('.');
 
             // get milliseconds
             var msec = 0;
-            if(decParts.Length == 2)
+            if (decParts.Length == 2)
             {
                 var msecPart = decParts[1];
-                while(msecPart.Length < 2)
+                while (msecPart.Length < 2)
                 {
                     msecPart += "0";
                 }
-                if(msecPart.Length > 0)
+                if (msecPart.Length > 0)
                 {
                     msecPart = msecPart.Substring(0, 2);
                 }
@@ -32,7 +82,7 @@ namespace TimeCalc.Services
             // calculate seconds from minutes
             var sec = 0;
             var secParts = decParts[0].Split(':');
-            if(secParts.Length == 1 && secParts[0].Length > 0)
+            if (secParts.Length == 1 && secParts[0].Length > 0)
             {
                 // seconds
                 sec = int.Parse(secParts[0]);
