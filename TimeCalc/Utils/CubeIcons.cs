8f812b48c00a220fc3b630188fadcc05b3572006
Michael Randers-Pehrson <michael.rp@gmail.com>
2020-05-25 12:08:53 -0400

    Add cube css icons

---
+++ TimeCalc/Utils/CubeIcons.cs
@@ -0,0 +1,52 @@
+namespace TimeCalc.Utils
+{
+    public class CubeIcons
+    {
+        public static string Css(string puzzle)
+        {
+            var iconCss = "";
+
+            switch (puzzle)
+            {
+                case "3x3":
+                    iconCss = "cubing-icon event-333";
+                    break;
+                case "2x2":
+                    iconCss = "cubing-icon event-222";
+                    break;
+                case "4x4":
+                    iconCss = "cubing-icon event-444";
+                    break;
+                case "5x5":
+                    iconCss = "cubing-icon event-555";
+                    break;
+                case "6x6":
+                    iconCss = "cubing-icon event-666";
+                    break;
+                case "7x7":
+                    iconCss = "cubing-icon event-777";
+                    break;
+                case "3x3 OH":
+                    iconCss = "cubing-icon event-333oh";
+                    break;
+                case "Clock":
+                    iconCss = "cubing-icon event-clock";
+                    break;
+                case "Megaminx":
+                    iconCss = "cubing-icon event-minx";
+                    break;
+                case "Pyraminx":
+                    iconCss = "cubing-icon event-pyram";
+                    break;
+                case "Skewb":
+                    iconCss = "cubing-icon event-skewb";
+                    break;
+                case "Square-1":
+                    iconCss = "cubing-icon event-sq1";
+                    break;
+            }
+
+            return iconCss;
+        }
+    }
+}
