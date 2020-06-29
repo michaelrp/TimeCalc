bd75741d2f5096773a7d51b51a61db1b13698a1c
Michael Randers-Pehrson <michael.rp@gmail.com>
2020-06-29 10:04:58 -0400

    Remove obsolete code
    Remove ConvertToCentiseconds

--- TimeCalc/Services/SolveCalculator.cs
+++ TimeCalc/Services/SolveCalculator.cs
@@ -137,44 +137,5 @@ namespace TimeCalc.Services
 
             return result;
         }
-
-        [Obsolete]
-        public int ConvertToCentiseconds(string input)
-        {
-            var decParts = input.Split('.');
-
-            // get milliseconds
-            var msec = 0;
-            if (decParts.Length == 2)
-            {
-                var msecPart = decParts[1];
-                while (msecPart.Length < 2)
-                {
-                    msecPart += "0";
-                }
-                if (msecPart.Length > 0)
-                {
-                    msecPart = msecPart.Substring(0, 2);
-                }
-                msec = int.Parse(msecPart);
-            }
-
-            // calculate seconds from minutes
-            var sec = 0;
-            var secParts = decParts[0].Split(':');
-            if (secParts.Length == 1 && secParts[0].Length > 0)
-            {
-                // seconds
-                sec = int.Parse(secParts[0]);
-            }
-            else if (secParts.Length == 2)
-            {
-                // minutes, seconds
-                sec = int.Parse(secParts[0]) * 60 + int.Parse(secParts[1]);
-            }
-
-            var result = (sec * 100) + msec;
-            return result;
-        }
     }
 }
