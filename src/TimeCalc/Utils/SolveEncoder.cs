9468207c812b2c6e83d4e8a98d8dd45163053c78
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-04-22 00:21:53 -0400

    Add encoder and sms link (#51)

---
+++ src/TimeCalc/Utils/SolveEncoder.cs
@@ -0,0 +1,76 @@
+using System.Text;
+using System.Web;
+using TimeCalc.Models;
+using TimeCalc.Services;
+
+namespace TimeCalc.Utils
+{
+    public class SolveEncoder
+    {
+        private const string NA = " - ";
+
+        public static string ToUrlEncode(PuzzleRound round, SolveCalculations calculations)
+        {
+            if (round == null)
+            {
+                return "";
+            }
+
+            var builder = new StringBuilder();
+
+            builder.Append(round.Puzzle);
+            if(!string.IsNullOrEmpty(round.Name))
+            {
+                builder.Append("%20");
+                builder.Append(round.Name.Replace(" ", "%20"));
+            }
+            builder.Append("%0a");
+
+            builder.Append("-----%0a");
+            
+            foreach(var solve in round.Solves)
+            {
+                builder.Append(solve.Number);
+                builder.Append(":%20");
+                builder.Append(solve.Result);
+                builder.Append("%0a");
+            }
+
+            builder.Append("-----%0a");
+
+            var hasCurrentAverage = calculations.CurrentAverage != NA;
+            var hasNeededForNewPB = calculations.NeededForNewPB != NA;
+            var hasBPA = calculations.BestPossibleAverage != NA;
+
+            if (hasCurrentAverage)
+            {
+                builder.Append("Curr%20");
+                builder.Append(calculations.CurrentAverage);
+
+                if (hasNeededForNewPB || hasBPA)
+                {
+                    builder.Append(",%20");
+                }
+            }
+
+            if (hasNeededForNewPB)
+            {
+                builder.Append("For%20PB%20");
+                builder.Append(calculations.NeededForNewPB);
+
+                if (hasBPA)
+                {
+                    builder.Append(",%20");
+                }
+            }
+
+            if (hasBPA)
+            {
+                builder.Append("BPA%20");
+                builder.Append(calculations.BestPossibleAverage);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
