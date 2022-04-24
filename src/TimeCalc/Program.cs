82b43579bb7a54f2b5f16eab79143de3fb48bbee
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-04-24 15:29:51 -0400

    Versioning in Program attribute

--- src/TimeCalc/Program.cs
+++ src/TimeCalc/Program.cs
@@ -1,7 +1,10 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TimeCalc;
 using TimeCalc.Services;
+
+[assembly:AssemblyVersion("1.1.0.*")]
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
