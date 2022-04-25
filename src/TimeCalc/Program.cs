283103cd558a9b5e5668c55c6913bde272483ed8
Michael Randers-Pehrson <michael.rp@gmail.com>
2022-04-24 21:23:35 -0400

    Shorten defined version to 1.1.*

--- src/TimeCalc/Program.cs
+++ src/TimeCalc/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TimeCalc;
 using TimeCalc.Services;
 
-[assembly:AssemblyVersion("1.1.0.*")]
+[assembly:AssemblyVersion("1.1.*")]
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
